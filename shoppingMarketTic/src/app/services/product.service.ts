a5def41ab81b4edd726777702b2c7da2680dc156
Cesar Augusto <cesinhalius@hotmail.com>
2024-10-10 17:14:05 -0300

    Organizando o codigo

--- shoppingMarketTic/src/app/services/product.service.ts
+++ shoppingMarketTic/src/app/services/product.service.ts
@@ -1,25 +1,36 @@
-import http from "../../http-common";
 import { ProductProps } from "../interfaces/Product";
+import http from "../../http-common";
 
-const findAll = async(type: string) => {
-	if(type){
-		const response = await http.get<ProductProps[]>("products/?_sort=price");
-		return type === "desc" ? response.data.reverse() : response.data;
-	}
-	
-	
-	const resp = await http.get<ProductProps[]>("products");
-	return resp.data
-}
+const findAll = async (type: string) => {
+  if (type) {
+    const response = await http.get<ProductProps[]>(`/products?_sort=price`);
 
-const searchName = async(name: string) => {
-	const response = await http.get<ProductProps[]>(`products?name=${name}`);
-	return response.data
-}
+    return type === "desc" ? response.data.reverse() : response.data;
+  }
+  const response = await http.get<ProductProps[]>("/products");
+  return response.data;
+};
+
+const findById = async (id: number) => {
+  const response = await http.get<ProductProps>(`/products/${id}`);
+  return response.data;
+};
+
+const findByName = async (name: string) => {
+  const response = await http.get<ProductProps[]>(`/products?name=${name}`);
+  return response.data;
+};
+
+const findSortPrice = async () => {
+  const response = await http.get<ProductProps[]>(`/products?_sort=price`);
+  return response.data;
+};
 
 const ProductService = {
-	findAll,
-	searchName
-}
+  findAll,
+  findById,
+  findByName,
+  findSortPrice,
+};
 
 export default ProductService;
